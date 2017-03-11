c435ec4e7bbe45b7f56c214476558741612cdef7
Vortex <corentin.lepez@outlook.com>
2017-03-11 19:27:46 +0100

    corentin

--- restaurateurs.php
+++ restaurateurs.php
@@ -8,9 +8,7 @@
   <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Open+Sans:600'>
 
       <link rel="stylesheet" href="css/style.css">
-<script>$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});</script>
+
 
 </head>
 
