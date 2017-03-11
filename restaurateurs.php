fc1765e07853e05b296623ee9743d0410240d93d
Vortex <corentin.lepez@outlook.com>
2017-03-11 19:01:53 +0100

    Update restaurateurs.php

--- restaurateurs.php
+++ restaurateurs.php
@@ -23,6 +23,15 @@
   
 
          <?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:meet-eat-sv-bdd.database.windows.net,1433; Database = Meet.Eat.-BDD", "Administrateur", "Corentin2017");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "Administrateur@meet-eat-sv-bdd", "pwd" => "Corentin2017", "Database" => "Meet.Eat.-BDD", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
@@ -32,23 +41,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
   if($conn === false)
 {
     die(print_r(sqlsrv_errors(), true));
-}
- if(!empty($_POST)) {
- try {
-     $name = $_POST['username'];
-     $password = $_POST['password'];
-     $sql_insert = "INSERT INTO client (user, password) 
-                    VALUES (?,?)";
-     $stmt = $conn->prepare($sql_insert);
-     $stmt->bindValue(1, $name);
-     $stmt->bindValue(2, $password);
-     $stmt->execute();
- }
- catch(Exception $e) {
-     die(var_dump($e));
- }
- echo "<h3>Your're registered!</h3>";
- }    ?>
+}   ?>
           
           <h1> Espace - Restaurateurs</h1>
       <form method="POST" action="restaurateurs.php" class="login-form">
